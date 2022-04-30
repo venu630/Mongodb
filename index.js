3572f939cf9cac85e5d67d9dee72f601b430ff98
venu630 <sirisanagandlavenu@gmail.com>
2022-04-30 22:08:40 +0530

    updated routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { connectToDb , getDb} = require('./db');
 
 const app = express();
+app.use(express.json());
+
 const port = 3000;
 
 //db connection
@@ -38,8 +40,49 @@ app.get("/books/:name", (req, res) => {
     db.collection("books")
     .findOne({name: req.params.name})
     .then((doc) => {
-        console.log(doc);
         res.send(doc);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+app.post("/books", (req, res) => {
+    
+    let book = req.body;
+    db.collection("books") 
+    .insertOne(book)
+    .then((result) => {
+        res.send(result);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+
+app.post("/books/:name", (req, res) => {
+
+    db.collection("books")
+    .deleteOne({name: req.params.name})
+    .then((doc) => {
+        res.send(doc);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+
+app.patch("/books/:name", (req, res) => {
+    
+    let updates = req.body;
+    //updates data => {"age":69}
+
+    db.collection("books") 
+    .updateOne({name: req.params.name}, {$set : updates})
+    .then((result) => {
+        res.send(result);
     })
     .catch(err => {
         console.log(err);
