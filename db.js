137d885b4fbed24f5ebfefd19b705a8f58f4efe9
venu630 <sirisanagandlavenu@gmail.com>
2022-05-01 10:50:59 +0530

    completed mongodb

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 const {MongoClient} = require('mongodb');
 
 let dbConnection
-
+const uri = "mongodb+srv://venu:nm78th2019@cluster0.mzcnu.mongodb.net/bookstore?retryWrites=true&w=majority"
 //cb callBack functin
 const connectToDb = (cb) => {
-    MongoClient.connect("mongodb://localhost:27017/bookstore")
+
+    //local connection
+    // MongoClient.connect("mongodb://localhost:27017/bookstore")
+    MongoClient.connect(uri)
+
     .then((client) => {
         dbConnection = client.db();
         return cb();
